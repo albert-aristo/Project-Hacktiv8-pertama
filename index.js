e07fe8c8336558b00a41c0b18d67eedbb2d1e318
Aristo <70944448+albert-aristo@users.noreply.github.com>
2021-04-06 10:21:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,143 @@
-
-//pindah array ke dalam index
-const angka = [0,0,0,0]
-const pemisah = angka.join(' - ')
-let hasil = randomNumber(angka) //angka random sebelum tambah 1
-let soal = hasil.join(' - ')
-
-// console.log(hasil);
-let jawabanBanding = jawaban(hasil,1) //sesudah tambah 1
-let hasilAkhir = jawabanBanding.join(' - ')
-// console.log(jawabanBanding);
-//assign angka jadi 0000 dengan ' - '
-let inputAngka = document.getElementsByClassName('angka')[0].children[0]
-inputAngka.innerText = pemisah
-
-
-//ketika sudah di click tombol,maka assign 0000 jadi angka random sebelum tambah1
-let kirim = document.getElementById('kirim')
-kirim.addEventListener('click', gameStart)
-
+var jawabanAsliFungsi = ''
 let flag = false
-
-function gameStart() {
-    reset()
-    inputAngka.innerText = soal
-    setTimeout(function(){
-        inputAngka.innerText = hasilAkhir
-        if(flag === false){
-            alert('anda salah')
-        }
-    }, 3000)
-}
-    
-let submitButton = document.getElementById('kumpulJawaban')
-submitButton.addEventListener('click', function(){
-    let bandingkan = document.getElementById("angka").value
-    console.log(bandingkan);
-    let arrayBanding = jawabanBanding.join('')
-    
-    if(bandingkan == arrayBanding){
-        alert('anda betul')
-        flag = true
+//muncul angka random/soal
+function angkaRandom(){ //parameter1 = 1,waktu = 7000
+    let operator = document.getElementById('operasi').value
+    let batas = Number(document.getElementById('batasDigit').value)
+    let parameter1 = Number(document.getElementById('nilaiTambahan').value)
+    let contoh = [,,,].join(' - ')
+    let contoh1 = document.getElementsByClassName('box-jawaban')[0]
+    for(let a = 0; a < contoh1.length; a++){
+        contoh1[a].setAttribute('maxlength',batas)
     }
-    // console.log(bandingkan, '===', jawabanBanding);
-})
 
-function reset(){
-    inputAngka.innerText = pemisah
-    hasil = randomNumber(angka)
-    soal = hasil.join(' - ')
-    jawabanBanding = jawaban(hasil,1)
-    hasilAkhir = jawabanBanding.join(' - ')
-    flag = false
-}
-//return angka random
-function randomNumber(angka){
-    let nampung = []
-    for(let a = 0; a < angka.length; a++){
-        nampung.push(Math.floor(Math.random()*10))
-    }
-    return nampung
-}
-//jawaban
-function jawaban(input, level){
+    document.getElementById('first').value = ''
+    document.getElementById('second').value = ''
+    document.getElementById('third').value = ''
+    document.getElementById('fourth').value = ''
+    document.getElementById('angkaDigit').innerText = contoh
     let output = []
+    let kali = 1
+    if(batas === 1){ //batas
+        kali = 10
+    }else if(batas === 2){
+        kali = 100
+    }else if(batas === 3){
+        kali = 1000
+    }
+    for(let a = 0; a < 4; a++){
+        output.push(Math.floor(Math.random() * kali))
+    }
+
+    let result = output.join(' - ')
+    document.getElementById('angkaDigit').innerText = result
+    jawabanAngkaRandom(parameter1,operator,output,batas)
+}
+// angkaRandom(1)
+
+//dapat jawaban
+function jawabanAngkaRandom(parameter1 = 1,operator = 'addition',input,batas = 1){
+    let result = []
+    let limit = 9
+    let waktu = Number(document.getElementById('batasWaktu').value)
+    if(batas === 1){
+        limit = 9
+    }else if(batas === 2){
+        limit = 99
+    }else if(batas === 3){
+        limit = 999
+    }
+
     for(let a = 0; a < input.length; a++){
-        if(input[a] + level > 9){
-            output.push(0)
-        }else{
-            output.push(input[a]+ level)
+        let jawaban = 0
+        if(operator === 'addition'){
+            jawaban = input[a] + parameter1
+            if(jawaban > limit){
+                result.push(0)
+            }else{
+                result.push(jawaban)
+            }
+        }else if(operator === 'divide'){
+            result.push(Math.abs(input[a] / parameter1))
+        }else if(operator === 'multiplying'){
+            jawaban = input[a] * parameter1
+            if(jawaban > limit){
+                result.push(0)
+            }else{
+                result.push(jawaban)
+            }
+        }else if(operator === 'subtract'){
+            jawaban = input[a] - parameter1
+            if(jawaban < 0){
+                result.push(0)
+            }else{
+                result.push(jawaban)
+            }
+        }else if(operator === 'Modulo'){
+            result.push(input[a] % parameter1)
         }
     }
-    return output
+
+
+    jawabanAsliFungsi = result
+    let jawaban = result.join(' - ')
+    let jawabanSoal = ''
+    for(let b = 0; b < result.length; b++){
+        jawabanSoal += result[b]
+    }
+    setTimeout(function(){
+        document.getElementById('angkaDigit').innerText = jawaban
+        let inputArray = []
+        inputArray.push(Number(document.getElementById('first').value))
+        inputArray.push(Number(document.getElementById('second').value))
+        inputArray.push(Number(document.getElementById('third').value))
+        inputArray.push(Number(document.getElementById('fourth').value))
+        let jawabanAku = ''
+        for(let a = 0; a < inputArray.length; a++){
+            jawabanAku += inputArray[a]
+        }
+        if(flag === false){
+            if(jawabanAku === jawabanSoal){
+                alert('betul')
+            }else{
+                alert('salah')
+            }
+        }else{
+            flag = false
+        }
+    },waktu)
+
+}
+
+//untuk masuk kan jawaban ke dalam array
+function inputJawaban(a){
+    a.preventDefault()
+    let input = []
+    input.push(document.getElementById('first').value)
+    input.push(document.getElementById('second').value)
+    input.push(document.getElementById('third').value)
+    input.push(document.getElementById('fourth').value)
+    cekJawaban(input,jawabanAsliFungsi)
+}
+
+function cekJawaban(jawabanInput,jawabanAsli){
+    let isiA = jawabanAsli.join('')
+    let isiB = jawabanInput.join('')
+    flag = true
+    if(isiA === isiB){
+        alert('betul')
+    }else{
+        alert('salah')
+    }
+}
+
+let count = 0
+function clickEvent(first,last){
+    let batas = Number(first.getAttribute('maxlength'))
+    count++
+    console.log(count,batas);
+    if(count === batas){
+        document.getElementById(last).focus()
+        count = 0
+    }
 }
